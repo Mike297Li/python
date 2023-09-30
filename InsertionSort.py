e5ed86eb11f8398c91cb139dd76ae1fcbcc1a101
lifuchun <01118311@wisedu.com>
2023-09-30 17:49:25 -0400

    insertion sort

---
+++ InsertionSort.py
@@ -0,0 +1,14 @@
+def selectionSort(arr):
+    for pointer in range(1, len(arr)):
+        position = pointer
+        currentVal = arr[pointer]
+        while position > 0 and arr[position - 1] > currentVal:
+            arr[position] = arr[position - 1]
+            position -= 1
+        arr[position] = currentVal
+    return arr
+
+
+print(selectionSort([2, 1, 3, 9, 8, 5]))
+# my_arr=[2,1,3,6,8,5]
+# print(len(my_arr))
