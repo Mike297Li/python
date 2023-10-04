331cbdecca976e789fce6b8bfbc7d4d94c63b0af
lifuchun <01118311@wisedu.com>
2023-10-03 20:54:20 -0400

    place holder

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+print (x)
+print (y)
+
+print ("I said: %r." % x)
+print ("I also said: '%s'." % y)
+
+text = "This is a 'sample' string."
+formatted_r = "Formatted string using %r: %r" % ("r", text)
+print(formatted_r)
